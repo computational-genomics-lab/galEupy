f1e3d9b03cdb334198cb0b570f3be476d3a7b26a
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2024-12-02 18:04:36 +0530

    Create upload_genomes_pipeline.sh

---
+++ upload_genomes_pipeline.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# A pipeline to upload organism data using galEupy to MySQL backend
+
+version=1
+declare -A uploaded_organisms # Associative array to track already uploaded organisms
+
+for file in *.fna; do
+    # Get the base name of the file (without the extension)
+    base=$(basename "$file" .fna)
+
+    # Get the header line from the file. This is the name of the organism
+    header=$(grep '^>' "$file" | head -n 1)
+
+    # Extract the organism name
+    organism_name=$(echo "$header" | awk -F' ' '/>/{print $2, $3}')
+
+    # Extract the strain name
+    strain_name=$(echo "$header" | awk -F'strain ' '{print $2}' | awk '{print $1}')
+
+    gff_file="${base}_with_product_name.gff3"
+    eggnog_file="${base}_eggnog.emapper.annotations"
+
+    echo "File: $file"
+    echo "Base name: $base"
+    echo "Organism name: $organism_name"
+    echo "Strain name: $strain_name"
+    echo "gff file: $gff_file"
+    echo "eggnog_file: $eggnog_file"
+    echo "version: $version"
+    echo "-----------------------------"
+
+    # Check if the header (ie the organism name) has been seen before
+    if [[ -n "${uploaded_organisms[$header]}" ]]; then
+        # Organism seen before, increment version
+        version=$((version + 1))
+    else
+        # New organism, record it
+        uploaded_organisms["$header"]=1
+    fi
+
+    # Making the organism.ini configuration file
+    cat <<EOF > organism.ini
+[OrganismDetails]
+Organism: $organism_name
+version: $version
+source_url:
+strain: $strain_name
+assembly_version: 1
+
+[SequenceType]
+SequenceType: chromosome
+scaffold_prefix:
+
+[filePath]
+GenBank:
+FASTA: $file
+GFF: $gff_file
+eggnog: $eggnog_file
+
+EOF
+
+    # Upload using galEupy
+    # It is assumed that a database.ini file is already present in the working directory
+    galEupy -db database.ini -org organism.ini -v d -upload All -log $strain.log
+done
