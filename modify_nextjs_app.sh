3b3f7b625e1a0157eed432bdf9991fd4a15bd863
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2024-12-06 15:55:16 +0530

    Update modify_nextjs_app.sh
    changed ./ to bash. otherwise the user would want to convert all the bash scripts into an executable format beforehand.

--- modify_nextjs_app.sh
+++ modify_nextjs_app.sh
@@ -92,12 +92,12 @@ npm install -g @jbrowse/cli #for installing cli version of jbrowse2
 
 
 #creating the index files
-./index_files.sh
+bash index_files.sh
 #moving
 mv genomes P_melonis_web_app/public/
 
 #prepare the config file for jbrowse2 visualisation
-./P_melonis_web_app/pages/components/visualization/track_adder.sh
+bash P_melonis_web_app/pages/components/visualization/track_adder.sh
 
 #Replace the ip address and the port
 #bash P_melonis_web_app/string_replace.sh "./test" "eumicrobedb.org" "http://$IP_ADDRESS:$PORT"
