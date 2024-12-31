6b943a8295e31987c23af515e91e2d7db358b4f2
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2024-12-31 12:42:14 +0530

    Update upload_genomes_pipeline.sh
    added qc option for FASTA files

--- upload_genomes_pipeline.sh
+++ upload_genomes_pipeline.sh
@@ -5,6 +5,10 @@
 version=1
 declare -A uploaded_organisms # Associative array to track already uploaded organisms
 
+#LOOP THROUGH FASTA FILES AND CHECK IF THEY ARE VALID OR NOT
+#if they are not valid : skip
+#if valid extract the organism name, strain name 
+
 for file in genomes/*.fna; do
     # Get the base name of the file (without the extension)
     base=$(basename "$file" .fna)
@@ -12,17 +16,30 @@ for file in genomes/*.fna; do
     # Get the header line from the file. This is the name of the organism
     header=$(grep '^>' "$file" | head -n 1)
 
-    # Extract the organism name
+    # Validate if a header exists
+    if [ -z "$header" ]; then
+        echo "Invalid FASTA file format: No header found in $file; skipping."
+        continue
+    fi
+
+    # Extract the organism name (using the second and third fields as default format)
     organism_name=$(echo "$header" | awk -F' ' '/>/{print $2, $3}')
 
-    # Check if the string "strain" is in the header
+    # Detect if the header contains the word "strain"
     if echo "$header" | grep -q "strain"; then
         # Extract the strain name
         strain_name=$(echo "$header" | awk -F'strain ' '{print $2}' | awk '{print $1}')
     else
-        # Extract the fourth word (after $3) if "strain" is not present
+        # Try extracting the fourth word as the strain name if "strain" is not present
         strain_name=$(echo "$header" | awk '{print $4}')
+        
+        # Validate the extracted strain name
+        if [ -z "$strain_name" ]; then
+            echo "Invalid FASTA file format in $file: Header does not match expected formats; skipping."
+            continue
+        fi
     fi
+# ............................END................................................
 
     gff_file="${base}_with_product_name.gff3"
     eggnog_file="${base}_eggnog.emapper.annotations"
